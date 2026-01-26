84d0f91ed2c876d2957685c691a06c2c607bbf7c
Tara Drwenski <drwenski1@llnl.gov>
2026-01-26 09:02:13 -0800

    Add enums to nanobind

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -12,6 +12,81 @@ namespace nb = nanobind;
 NB_MODULE(quandary_ext, m) {
   m.doc() = "Quandary Python bindings";
 
+  // Enums
+  nb::enum_<DecoherenceType>(m, "DecoherenceType")
+    .value("NONE", DecoherenceType::NONE)
+    .value("DECAY", DecoherenceType::DECAY)
+    .value("DEPHASE", DecoherenceType::DEPHASE)
+    .value("BOTH", DecoherenceType::BOTH);
+
+  nb::enum_<InitialConditionType>(m, "InitialConditionType")
+    .value("FROMFILE", InitialConditionType::FROMFILE)
+    .value("PRODUCT_STATE", InitialConditionType::PRODUCT_STATE)
+    .value("ENSEMBLE", InitialConditionType::ENSEMBLE)
+    .value("DIAGONAL", InitialConditionType::DIAGONAL)
+    .value("BASIS", InitialConditionType::BASIS)
+    .value("THREESTATES", InitialConditionType::THREESTATES)
+    .value("NPLUSONE", InitialConditionType::NPLUSONE)
+    .value("PERFORMANCE", InitialConditionType::PERFORMANCE);
+
+  nb::enum_<TargetType>(m, "TargetType")
+    .value("NONE", TargetType::NONE)
+    .value("GATE", TargetType::GATE)
+    .value("STATE", TargetType::STATE);
+
+  nb::enum_<ObjectiveType>(m, "ObjectiveType")
+    .value("JFROBENIUS", ObjectiveType::JFROBENIUS)
+    .value("JTRACE", ObjectiveType::JTRACE)
+    .value("JMEASURE", ObjectiveType::JMEASURE);
+
+  nb::enum_<LinearSolverType>(m, "LinearSolverType")
+    .value("GMRES", LinearSolverType::GMRES)
+    .value("NEUMANN", LinearSolverType::NEUMANN);
+
+  nb::enum_<RunType>(m, "RunType")
+    .value("SIMULATION", RunType::SIMULATION)
+    .value("GRADIENT", RunType::GRADIENT)
+    .value("OPTIMIZATION", RunType::OPTIMIZATION)
+    .value("EVALCONTROLS", RunType::EVALCONTROLS)
+    .value("NONE", RunType::NONE);
+
+  nb::enum_<ControlType>(m, "ControlType")
+    .value("NONE", ControlType::NONE)
+    .value("BSPLINE", ControlType::BSPLINE)
+    .value("BSPLINEAMP", ControlType::BSPLINEAMP)
+    .value("BSPLINE0", ControlType::BSPLINE0);
+
+  nb::enum_<ControlInitializationType>(m, "ControlInitializationType")
+    .value("CONSTANT", ControlInitializationType::CONSTANT)
+    .value("RANDOM", ControlInitializationType::RANDOM)
+    .value("FILE", ControlInitializationType::FILE);
+
+  nb::enum_<TimeStepperType>(m, "TimeStepperType")
+    .value("IMR", TimeStepperType::IMR)
+    .value("IMR4", TimeStepperType::IMR4)
+    .value("IMR8", TimeStepperType::IMR8)
+    .value("EE", TimeStepperType::EE);
+
+  nb::enum_<GateType>(m, "GateType")
+    .value("NONE", GateType::NONE)
+    .value("XGATE", GateType::XGATE)
+    .value("YGATE", GateType::YGATE)
+    .value("ZGATE", GateType::ZGATE)
+    .value("HADAMARD", GateType::HADAMARD)
+    .value("CNOT", GateType::CNOT)
+    .value("SWAP", GateType::SWAP)
+    .value("SWAP_0Q", GateType::SWAP_0Q)
+    .value("CQNOT", GateType::CQNOT)
+    .value("QFT", GateType::QFT)
+    .value("FILE", GateType::FILE);
+
+  nb::enum_<OutputType>(m, "OutputType")
+    .value("EXPECTED_ENERGY", OutputType::EXPECTED_ENERGY)
+    .value("EXPECTED_ENERGY_COMPOSITE", OutputType::EXPECTED_ENERGY_COMPOSITE)
+    .value("POPULATION", OutputType::POPULATION)
+    .value("POPULATION_COMPOSITE", OutputType::POPULATION_COMPOSITE)
+    .value("FULLSTATE", OutputType::FULLSTATE);
+
   // Config class
   nb::class_<Config>(m, "Config")
     // fromFile static method
