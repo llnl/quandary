729894eb74cb43594a0079927769235bbf014d6d
steffi7574 <steffi7574@users.noreply.github.com>
2026-07-17 19:23:40 +0000

    deploy: 39576ead1a4d5e7ec71b267ccfb0564e27187c0c

--- doxygen/search/functions_e.js
+++ doxygen/search/functions_e.js
@@ -3,6 +3,6 @@ var searchData=
   ['opentrajectorydatafiles_0',['openTrajectoryDataFiles',['../classOutput.html#a9a9b7c2e998af8d181ffe315f70a56cf',1,'Output']]],
   ['optimproblem_1',['OptimProblem',['../classOptimProblem.html#a04998829e4abd47cf6057e780791eec3',1,'OptimProblem']]],
   ['optimtarget_2',['optimtarget',['../classOptimTarget.html#ad4e0df2f3df0dfdf8ef8ecbfd299ca5a',1,'OptimTarget::OptimTarget()'],['../classOptimTarget.html#aab10681bf201570296e40579805d4b36',1,'OptimTarget::OptimTarget(const Config &amp;config, MasterEq *mastereq, bool quietmode_)']]],
-  ['oscillator_3',['oscillator',['../classOscillator.html#a99fd3a4a9ef26a4f8025ac7a519d900b',1,'Oscillator::Oscillator()'],['../classOscillator.html#acb40f6527b94d6e7718b3e40ab18c636',1,'Oscillator::Oscillator(const Config &amp;config, size_t id, std::mt19937 rand_engine, int param_offset, bool quietmode)']]],
+  ['oscillator_3',['oscillator',['../classOscillator.html#a99fd3a4a9ef26a4f8025ac7a519d900b',1,'Oscillator::Oscillator()'],['../classOscillator.html#a2401d4a510aa7deeec17c41f6330a837',1,'Oscillator::Oscillator(const Config &amp;config, size_t id, std::mt19937 &amp;rand_engine, int param_offset, bool quietmode)']]],
   ['output_4',['output',['../classOutput.html#a428c663520336477a12f1a33504eb067',1,'Output::Output()'],['../classOutput.html#a5a10b4cdb31f2fe1705bfdec8f875bcd',1,'Output::Output(const Config &amp;config, MPI_Comm comm_petsc, MPI_Comm comm_init, bool quietmode=false)']]]
 ];
